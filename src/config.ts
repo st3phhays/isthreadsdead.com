44ace491e1e424e24e95e1def001de9b3384fd40
st3phhays <steph@stephhays.dev>
2023-07-08 07:46:15 -0500

    Add <marquee>
    Adds 100 <marquee>'s, because why not?

---
+++ src/config.ts
@@ -0,0 +1,7 @@
+export const SITE = {
+    author: "Steph Hays",
+    title: 'Is Threads Dead?',
+    description: 'Is Threads Dead? - The go-to website that digs deep into the digital graveyard to determine whether Threads is still stitching conversations together or if it has become a mere spool of forgotten threads, all with a straightforward yes or no.',
+    url: import.meta.env.SITE,
+    gitHubUrl: 'https://github.com/st3phhays/isthreadsdead.com',
+};
